6122adf746c81747289db6bd4b93ea0ad1753155
Ashley Lamont <ashley@ashl.dev>
2024-10-17 10:16:31 +1100

    Add vote count

--- app/routes/leaderboard.tsx
+++ app/routes/leaderboard.tsx
@@ -8,16 +8,27 @@ import { prisma } from "~/db.server";
 export async function loader() {
   const quotes = await prisma.quote.findMany();
   quotes.sort((a, b) => ordinal(b) - ordinal(a));
-  return json(quotes);
+  const voteCount = await prisma.match.count({
+    where: {
+      winnerMessageId: {
+        not: null,
+      },
+    },
+  });
+  return json({ quotes, voteCount });
 }
 
 export default function Leaderboard() {
-  const quotes = useLoaderData<typeof loader>().map((quote) => ({
+  const { quotes: _quotes, voteCount } = useLoaderData<typeof loader>();
+  const quotes = _quotes.map((quote) => ({
     ...quote,
     timestamp: new Date(quote.timestamp),
   }));
   return (
     <div className="flex flex-col gap-2">
+      <h1 className="text-2xl text-white">
+        Leaderboard (based on {voteCount} votes)
+      </h1>
       {quotes.map((quote, index) => (
         <LeaderboardEntry
           key={quote.messageId}
